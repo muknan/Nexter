f8f1f6c61f5c7bae050741d2ceabd6f6d52117b3
muknan <muk.msi@gmail.com>
2023-08-25 13:01:22 +0530

    Updates

--- js/script.js
+++ js/script.js
@@ -40,3 +40,25 @@ sidebarEl.addEventListener(
   },
   false
 );
+
+////////////////////////////////////////////////////////////
+//Implementing smooth scrolling animation
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    // e.preventDefault();
+    const href = link.getAttribute("href");
+    //Scroll back to top
+    if (href === "#") {
+      e.preventDefault();
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }
+
+    //Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      e.preventDefault();
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
