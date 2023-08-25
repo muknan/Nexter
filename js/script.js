af369e345e75bf9b16679c37e3c8dfdd01255246
muknan <muk.msi@gmail.com>
2023-08-25 12:21:56 +0530

    Finising up the sidebar

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const likeEl = document.querySelectorAll(".home__like");
+const sidebarEl = document.querySelector(".sidebar");
 
 likeEl.forEach((like) => {
   like.addEventListener("click", function () {
@@ -6,3 +7,36 @@ likeEl.forEach((like) => {
     like.classList.toggle("like-toggle"), false;
   });
 });
+
+const btnNavEl = document.querySelector(".nav-cb");
+
+function uncheckBox() {
+  if (btnNavEl.checked) {
+    btnNavEl.checked = false;
+  }
+}
+
+const navEl = document.querySelectorAll(".nav__items");
+navEl.forEach(function (item) {
+  item.addEventListener("click", function () {
+    uncheckBox();
+  });
+});
+
+const bodyEl = document.querySelector("body");
+
+bodyEl.addEventListener(
+  "click",
+  function () {
+    uncheckBox();
+  },
+  false
+);
+
+sidebarEl.addEventListener(
+  "click",
+  function (ev) {
+    ev.stopPropagation();
+  },
+  false
+);
